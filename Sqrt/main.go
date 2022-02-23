e430e971eeb6e15a20e46b51cea030ad9b75c5f3
Art2310 <art.sanches.os@gmail.com>
2022-02-23 18:06:16 -0300

    Neg Sqrt

--- Sqrt/main.go
+++ Sqrt/main.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 )
 
-func Sqrt(x float64) float64 {
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
 	z := float64(1)
 	y := float64(0)
 	for i := 1; i <= 10; i++ {
@@ -15,10 +24,10 @@ func Sqrt(x float64) float64 {
 		}
 		fmt.Println(z)
 	}
-	return z
-
+	return z, nil
 }
 
 func main() {
-	Sqrt(4)
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
 }
